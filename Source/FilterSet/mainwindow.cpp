203ad1cba5ff2842eb967f31200057c67aa50350
fanck13 <fanckai13@hotmail.com>
2020-07-20 00:13:04 +0800

    show picture

--- Source/FilterSet/mainwindow.cpp
+++ Source/FilterSet/mainwindow.cpp
@@ -1,12 +1,20 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QLabel>
+#include <QPixmap>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QLabel* pictureLabel = new QLabel(ui->centralwidget);
+
+    QImage *image= new QImage("D:\\FilterSet\\TestData\\elephants.jpg");
+    pictureLabel->setPixmap(QPixmap::fromImage(*image));
 }
+
+
 
 MainWindow::~MainWindow()
 {
